ec02fc3bbdf28eaa97ff3079409bcd6b09acd184
SamratChakraborty <csamrat53@gmail.com>
2023-09-07 18:21:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Fetching elements by id's
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+// on-click func for signin
+signinBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+}
+
+// on-click func for signup
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = "60px";
+    title.innerHTML = "Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+}
